ce9327a8d705f098291c8a20cc7675214370cefd
quizoverflow <jinuinssu@soongsil.ac.kr>
2023-11-01 17:03:53 +0900

    ps template

--- ps_template/basic_template.cpp
+++ ps_template/basic_template.cpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 #define FASTIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #ifndef ONLINE_JUDGE
-#include "debug.h"
+#include "debug.h" // 상위 폴더로 해두는게 좋음 ex) "..\headfiles\debug.h"
 #else
 #define debug(x...)
 #endif 
